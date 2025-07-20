9b42303fae02561b31478adb6b41ace7083d9050
Mohammed Nagdy <42354467+MohammedNagdy@users.noreply.github.com>
2025-07-20 12:06:31 +0000

    loops

---
+++ loops.py
@@ -0,0 +1,32 @@
+# Loops 
+# names = []
+
+# for element in names:
+#     result = "Hello " + element
+#     print(result)
+
+# for i in range(20):    
+#     # Print out even numbers
+#     if i % 2 == 0:
+#         print(i, " even")
+#     # Print out odd numbers
+#     else:
+#         print(i, " odd")
+# +
+# -
+# /
+# %
+# **
+# *
+
+# while if something is true then do the operation
+# number = 0
+# while number < 20:
+#     if number % 2 == 0:
+#         print(number, " even")
+#     else:
+#         print(number, " odd")
+#     number += 1
+
+
+
